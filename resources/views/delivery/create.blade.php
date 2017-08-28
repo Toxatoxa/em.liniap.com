e04447a0daa0fd1ce1f95fd9ac30c03cd19547b6
swarthytony <toxaphp@gmail.com>
2017-08-28 16:56:01 +0200

    tinymce

--- resources/views/delivery/create.blade.php
+++ resources/views/delivery/create.blade.php
@@ -81,5 +81,14 @@
     <link href="{{ asset('js/tinymce/skins/lightgray/content.min.css') }}" rel="stylesheet">
     <script src="{{ asset('js/tinymce/tinymce.min.js') }}"></script>
     <script src="{{ asset('js/tinymce/themes/modern/theme.min.js') }}"></script>
-    <script>tinymce.init({selector: 'textarea'});</script>
+    <script>tinymce.init({
+            selector: 'textarea',
+            menubar: false,
+            plugins: [
+                'advlist autolink lists link image charmap print preview anchor',
+                'searchreplace visualblocks code fullscreen',
+                'insertdatetime media table contextmenu paste code'
+            ],
+            toolbar: 'undo redo | insert | styleselect | bold italic | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | link image',
+    });</script>
 @endsection
